33d0e2eb3a1f3ca61736e8169e6aa021a2e50924
ZHAY10086 <ZHAY10086@outlook.com>
2023-05-10 17:55:14 +0800

    Rollback

--- firmware/LoRa_transmit/system/libraries/timer.h
+++ firmware/LoRa_transmit/system/libraries/timer.h
@@ -5,9 +5,6 @@
 bool timeout_flag;
 bool timerstart;
 
-// added by ZY
-extern bool flag_bc;
-
 /**#######################External functions######################**/
 void timerinitial(void);
 void settimer(void);
